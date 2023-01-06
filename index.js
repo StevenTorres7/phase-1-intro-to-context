525cc27ab68bbee5e5f65c365d2c55a2b0c5ab86
Steven <Steventorres7@yahoo.com>
2023-01-05 20:39:41 -0500

    commit tree 6d6d5dd9d9f95e2f83ceec8b57208dd08e1a5c87

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Your code here
+// // Your code here
+function createEmployeeRecord(row) {
+     return{
+        firstName: row[0],
+         familyName: row[1],
+         title: row[2],
+         payPerHour: row[3],
+         timeInEvents: [],
+         timeOutEvents: [],
+    }
+ }
+
+ let createEmployees = function(employeeRowData) {
+    let createEmployeeRecords = function(employeeRowData) {
+        return employeeRowData.map(function(row){
+            return createEmployeeRecord(row)
+        })
+    }
+}
+// let createEmployees = function(employeeRowData) {
+//     let createEmployeeRecords = function(employeeRowData) {
+//         return employeeRowData.map(function(row){
+//             return createEmployeeRecord(row)
+//         })
+//     }
+// }
+// function createTimeInEvent(employee, timeStamp) {
+//         let [date, hour] = timeStamp.split("")
+
+//         employee.timeInEvents.push({
+//             type: "TimeIn",
+//             hour: parseInt(hour, 10),
+//             date,
+//         })
+
+//         return employee;
+//     }
+
+//     function createTimeOutEvent(employee, timeStamp) {
+//         let [date, hour] = timeStamp.split("")
+
+//         employee.timeOutEvents.push({
+//             type: "TimeOut",
+//             hour: parseInt(hour, 10),
+//             date,
+//         })
+
+//         return employee;
+//     }
